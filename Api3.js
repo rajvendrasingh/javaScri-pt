db8aeb82e0eb19ca9c2bd46ad555a363da3535a1
rajvendrasingh <sseema7350@gmail.com>
2026-02-26 20:49:40 +0530

    Create Api3.js

---
+++ Api3.js
@@ -0,0 +1,24 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = true;
+
+      if (success) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Error fetching data.");
+      }
+    }, 2000);
+  });
+}
+
+async function getData() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
